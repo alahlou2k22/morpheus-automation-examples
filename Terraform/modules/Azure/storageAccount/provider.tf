eeecbdcfe9f45f2f74cd2b57ae920b950cda2fcb
alahlou2k22 <102142494+alahlou2k22@users.noreply.github.com>
2022-07-27 15:55:14 +0100

    Update provider.tf

--- Terraform/modules/Azure/storageAccount/provider.tf
+++ Terraform/modules/Azure/storageAccount/provider.tf
@@ -1,7 +1,9 @@
-terraform {
-   required_version = ">= 0.14.5"
-
-   required_providers {
-     azurerm = ">= 2.5.0"
-   }
+provider "azurerm" {
+  version = ">=2.5.0"
+  features {}
+  
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
